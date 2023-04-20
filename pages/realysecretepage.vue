9b7ff651b95c62f0d6d3130100b4e0a2d63ffd81
masterdpro <youri.d2.bel@gmail.com>
2023-04-20 19:42:00 -0400

    secret page

--- pages/realysecretepage.vue
+++ pages/realysecretepage.vue
@@ -1,3 +1,5 @@
 <template>
-    <h1>Oui</h1>
+    <h1>OMG</h1>
+    <h2>You are either lucky or you trust me, that's cool !</h2>
+    <h2>This page is useless for the moment, it just a test !</h2>
 </template>
