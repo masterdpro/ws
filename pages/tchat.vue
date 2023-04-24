3b5e72046e75f27796b2ed3c2a70b27ed7bebdd0
masterdpro <youri.d2.bel@gmail.com>
2023-04-24 18:51:39 -0400

    new chat page test

---
+++ pages/tchat.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+      <form @submit.prevent="sendMessage">
+        <label for="message">Type your message:</label>
+        <input type="text" id="message" v-model="newMessage" />
+        <button type="submit">Send</button>
+      </form>
+  
+      <h2>Messages:</h2>
+      <ul>
+        <li v-for="(message, index) in messages" :key="index">{{ message }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script lang="js">
+  export default {
+    data() {
+      return {
+        messages: [],
+        newMessage: "",
+      };
+    },
+  
+    methods: {
+      sendMessage() {
+        this.messages.push(this.newMessage);
+        this.newMessage = "";
+      },
+    },
+  };
+  </script>
+  
